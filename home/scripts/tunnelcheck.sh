24b5e4cecbfc6b984c33f73116ae4da447bf3faf
Daniel Loader <spicypixel@gmail.com>
2018-11-04 10:26:10 +0000

    Offered network device choice via argument

--- home/scripts/tunnelcheck.sh
+++ home/scripts/tunnelcheck.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 printf "Checking public IP\n"
 printf "%-15s" "Local"
-ip addr show wifi0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
+ip addr show "$1" | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
 printf "%-15s" "Gateway"
-ip route show dev wifi0 | fgrep default | sed 's/default via //' | awk '{print $2}'
+ip route show dev "$1" | fgrep default | sed 's/default via //' | awk '{print $2}'
 printf "%-15s" "SOCKS" 
 curl --proxy socks5h://localhost:1080 ifconfig.co
 printf "%-15s" "HTTP"
