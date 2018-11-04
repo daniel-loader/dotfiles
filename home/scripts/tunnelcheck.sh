d8304b13f62232dce9133f5902597d742dc4f71e
Daniel Loader <spicypixel@gmail.com>
2018-11-04 10:21:51 +0000

    More checking added to tunnelcheck

--- home/scripts/tunnelcheck.sh
+++ home/scripts/tunnelcheck.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-printf "Checking public proxy IPs\n"
-printf "SOCKS5: " 
+printf "Checking public IP\n"
+printf "%-15s" "Local"
+ip addr show wifi0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
+printf "%-15s" "Gateway"
+ip route show dev wifi0 | fgrep default | sed 's/default via //' | awk '{print $2}'
+printf "%-15s" "SOCKS" 
 curl --proxy socks5h://localhost:1080 ifconfig.co
-printf "HTTP: "
+printf "%-15s" "HTTP"
 curl --proxy 127.0.0.1:1081 ifconfig.co
-printf "Unproxied public IP: "
+printf "%-15s" "No Proxy"
 curl ifconfig.co
 exit
